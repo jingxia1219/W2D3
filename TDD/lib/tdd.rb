5e1b4e7ba9c3212bc8728950543ba5f85d626d08
Sam Williams <sbw2122@columbia.edu>
2018-06-27 14:37:10 -0700

    add two_sum and my_traponse methods and Rspecs

--- TDD/lib/tdd.rb
+++ TDD/lib/tdd.rb
@@ -1,9 +1,70 @@
 class Array
   
   def my_uniq
-    self.uniq
+    result = [] 
+    self.each {|el| result << el unless result.include?(el)}
+    result  
   end 
   
-  def to_s
+  def two_sum 
+    indices = [] 
+    i = 0 
+    while i < length - 1
+      j = i + 1
+      while j < length 
+        indices << [i, j] if self[i] + self[j] == 0
+        j += 1 
+      end 
+      i += 1 
+    end 
+    indices
   end 
+  
+  def my_transpose
+    rows, cols = self.length, self[0].length
+    result = Array.new(cols) {Array.new(rows)}
+    # i = 0 
+  #   while i < rows 
+  #     j = 0 
+  #     while j < cols
+  #       result[i][j] = self[j][i]
+  #       j +=1 
+  #   end 
+  #   i +=1 
+  # end 
+    self.each_with_index do |row, row_idx| 
+      row.each_with_index do |col, col_idx| 
+        result[col_idx][row_idx] = col 
+      end 
+    end 
+    result 
+  end 
+
+end 
+
+def stock_picker(arr)
+   # result = []
+   # profit = 0 
+   # arr.each_with_index do |el, idx|
+   #   break if idx == arr.length - 1
+   #   ((idx + 1)...arr.length).each do |idx2|
+   # 
+   # end 
+   # end 
+   hash = Hash.new 
+   i = 0 
+   while i < arr.length - 1
+     
+     j = i + 1
+     while j < arr.length 
+       hash[[i, j]] = arr[j] - arr[i]
+       j += 1
+     end 
+     
+     i += 1 
+   end 
+    
+   result = hash.sort_by {|_, v| v}
+   result.last.first 
+   
 end 
--- TDD/spec/tdd_spec.rb
+++ TDD/spec/tdd_spec.rb
@@ -9,5 +9,36 @@ require 'rspec'
     expect(arr.my_uniq).to eq([1, 2, 3])
   end 
  end 
+ 
+ describe '#two_sum' do 
+   it 'returns indexes of pairs that add to zero' do 
+    arr = [-1, 0, 2, -2, 1]
+    expect(arr.two_sum).to eq([[0, 4], [2, 3]])
+    end 
+  end 
+  
+  describe '#my_transpose' do 
+    it 'tranposes a matrix' do 
+      rows = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+        ]   
+        cols = [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8]
+        ]
+        expect(rows.my_transpose).to eq(cols)
+    end 
+  end 
+  
+end 
+
+describe '#stock_picker' do 
+  it 'picks the best day to buy and to sell to get the highest profit' do 
+    arr = [20,28,5,17,22]
+    expect(stock_picker(arr)).to eq([2,4]) 
+  end 
 end 
 
